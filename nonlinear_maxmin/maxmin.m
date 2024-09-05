2214af076b6441a251e398f574ba537ced914a39
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2024-09-05 12:04:16 +0530

    Add files via upload

--- nonlinear_maxmin/maxmin.m
+++ nonlinear_maxmin/maxmin.m
@@ -18,13 +18,25 @@ d=imnoise(a,'salt & pepper',0.2);
 subplot(3,3,4);imshow(d);
 title("Original with noise");
 
-e=ordfilt2(d,9,ones(3,3));
+e=ordfilt2(d,1,ones(3,3));
 subplot(3,3,5);imshow(e);
 title("min filter with noise");
 
 f=ordfilt2(d,9,ones(3,3));
 subplot(3,3,6);imshow(f);
 title("max filter with noise");
+
+g=imread("cameraman.tif");
+subplot(3,3,7);imshow(g);
+title("Original Image");
+
+h=ordfilt2(a,1,ones(5,5));
+subplot(3,3,8);imshow(h);
+title("Min filter 5x5");
+
+i=ordfilt2(a,9,ones(5,5));
+subplot(3,3,9);imshow(i);
+title("Max filter 5x5");
 
 
 
